f70500f727802205b7ce6368309ef96ed93926b7
hafsaa24 <hafsaharchiche99@gmail.com>
2023-03-16 16:50:49 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - main function
+ *
+ * return: should always be 0
+ */
+
+int main(void)
+{
+	puts ("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
