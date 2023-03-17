c09f10a7d96f8533d2d09b80476ede11a92eb56d
hafsaa24 <hafsaharchiche99@gmail.com>
2023-03-16 17:46:41 -0700

    sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  * main - starting point
+  *
+  * Return: 0 when successful
+  */
+
+int main(void)
+{
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+return (0);
+}
