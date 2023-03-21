bb253d906f194db0bfcf2379697f54cfc5d0de71
hafsaa24 <hafsaharchiche99@gmail.com>
2023-03-20 17:22:54 -0700

    combos

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,10 +12,11 @@ int main(void)
 
 	for (i = 0 ; i < 10 ; i++)
 	{
+		if (i == 9)
+			putchar(i + '0');
 		putchar(i + '0');
 		putchar(',');
 		putchar(' ');
 	}
-	putchar('\n');
 	return (0);
 }
