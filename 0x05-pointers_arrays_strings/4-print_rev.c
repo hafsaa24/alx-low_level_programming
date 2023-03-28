40e4da1958842bb1768656ed7bff9ae11082d929
hafsaa24 <hafsaharchiche99@gmail.com>
2023-03-28 13:54:27 -0700

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,7 +3,7 @@
 /**
 * print_rev - printing a string in reverse
 * @s: function parameter
-* Return:0
+* Return: 0
 */
 
 void print_rev(char *s)
@@ -11,9 +11,9 @@ void print_rev(char *s)
 	int i;
 	int count = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0 ; s[i] != '\0'; i++)
 		count++;
-	for (i = count ; i >= 0; i--)
-		_putchar(s[i]); i
+	for (i = count ; i >= 0 ; i--)
+		_putchar(s[i]);
 	_putchar('\n');
 }
